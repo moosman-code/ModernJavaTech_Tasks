652cd81d2984a6dca2ac7455e2580eb9431048c0
Martin Nikolov <martivdev38@gmail.com>
2025-05-06 13:22:48 +0300

    Rename project file

---
+++ MJT_Olympics/src/bg/sofia/uni/fmi/mjt/olympics/comparator/NationMedalComparator.java
@@ -0,0 +1,30 @@
+package bg.sofia.uni.fmi.mjt.olympics.comparator;
+
+import bg.sofia.uni.fmi.mjt.olympics.MJTOlympics;
+
+import java.util.Comparator;
+
+public class NationMedalComparator implements Comparator<String> {
+
+    private final MJTOlympics olympics;
+
+    public NationMedalComparator(MJTOlympics olympics) {
+        this.olympics = olympics;
+    }
+
+    @Override
+    public int compare(String nation1, String nation2) {
+        if (nation1 == null ||  nation2 == null) {
+            throw new IllegalArgumentException("Nations cannot be null");
+        }
+
+        int totalMedals1 = olympics.getTotalMedals(nation1);
+        int totalMedals2 = olympics.getTotalMedals(nation2);
+
+        if (totalMedals1 != totalMedals2) {
+            return Integer.compare(totalMedals1, totalMedals2);
+        }
+
+        return nation1.compareTo(nation2);
+    }
+}
